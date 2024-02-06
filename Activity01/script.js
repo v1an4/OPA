bd9524c41867caa21eeccdb2ed68f668d5412e53
v1an4 <vianahp@gmail.com>
2024-02-06 12:25:44 -0300

    subindo as coisas do prof no meu rep

---
+++ Activity01/script.js
@@ -0,0 +1,13 @@
+function somar() {
+    var num1 = document.getElementById("num1").valueAsNumber;
+    var num2 = document.getElementById("num2").valueAsNumber;
+    var res = num1 + num2;
+    document.getElementById("resultadoSoma").textContent = res.toFixed(2);
+}
+function sub(){
+  
+
+}
+
+
+
